2aa6080453c10ddb1e7b874029ced7738a4cfff5
Clarence Davis Jr <cldavisjr@gmail.com>
2018-01-16 12:27:01 -0600

    disable the signal until we can safely remove the block.

--- exchange/views.py
+++ exchange/views.py
@@ -656,8 +656,8 @@ def publish_service(request, pk):
     return redirect('services')
 
 
-@receiver(pre_delete, sender=Service,
-          dispatch_uid='remove_record_from_registry')
+#@receiver(pre_delete, sender=Service,
+#          dispatch_uid='remove_record_from_registry')
 def remove_record_from_csw(sender, instance, using, **kwargs):
     """
     Delete all csw records associated with the service. We only
@@ -670,7 +670,7 @@ def remove_record_from_csw(sender, instance, using, **kwargs):
         delete_record(instance.uuid)
 
 
-@receiver(post_save, sender=Service)
+#@receiver(post_save, sender=Service)
 def service_post_save(sender, **kwargs):
     """
     Assign CSW Manager permissions for all newly created Service instances.
