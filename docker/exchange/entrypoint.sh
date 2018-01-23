63bb838055753916171094b9ad9eb350b0f877e4
Daniel Berry <dberry@boundlessgeo.com>
2018-01-22 20:18:35 -0600

    maploom dev hotfix

--- docker/exchange/entrypoint.sh
+++ docker/exchange/entrypoint.sh
@@ -4,8 +4,31 @@ set -e
 
 manage='python /code/manage.py'
 setup='python /code/setup.py'
-
+maploom_static='/code/exchange/maploom/templates/maploom'
+maploom_templates='exchange/maploom/templates'
 # let the db intialize
+if [[ $MAPLOOM_DEV == True ]]; then
+  if [[ -d $maploom_static/assets ]]; then
+    rm -r $maploom_static/assets
+  fi
+  ln -sf /code/vendor/maploom/bin/assets $maploom_static/assets
+  if [[ -d $maploom_static/fonts ]]; then
+    rm -r $maploom_static/fonts
+  fi
+  ln -sf /code/vendor/maploom/bin/fonts $maploom_static/fonts
+  if [[ -f $maploom_templates/maploom/_maploom_map.html ]]; then
+    rm $maploom_templates/maploom/_maploom_map.html
+  fi
+  ln -sf /code/vendor/maploom/bin/_maploom_map.html $maploom_templates/maploom/_maploom_map.html
+  if [[ -f $maploom_templates/maploom/_maploom_map.html ]]; then
+    rm $maploom_templates/maploom/_maploom_map.html
+  fi
+  ln -sf /code/vendor/maploom/bin/_maploom_map.html $maploom_templates/maploom/_maploom_map.html
+  if [[ -f $maploom_templates/maps/maploom.html ]]; then
+    rm $maploom_templates/maploom/_maploom_map.html
+  fi
+  ln -sf /code/vendor/maploom/bin/_maploom_map.html $maploom_templates/maps/maploom.html
+fi
 sleep 15
 until $manage migrate account --noinput; do
   >&2 echo "db is unavailable - sleeping"
